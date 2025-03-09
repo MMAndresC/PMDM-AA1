292be08b3f9da78261015bbb190239468d40c382
Mar <mandrescasabon@hotmail.com>
2025-03-09 18:29:06 +0100

    :recycle: create validate util

---
+++ app/src/main/java/com/svalero/tournaments/util/ValidateUtil.java
@@ -0,0 +1,41 @@
+package com.svalero.tournaments.util;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.svalero.tournaments.R;
+import com.svalero.tournaments.domain.Tournament;
+
+import java.text.ParseException;
+
+public class ValidateUtil {
+
+    public static Tournament validateTournamentForm(View view) throws NullPointerException, NumberFormatException{
+        try {
+            String name = ((EditText) view.findViewById(R.id.editTournamentName)).getText().toString();
+            String manager = ((EditText) view.findViewById(R.id.editTournamentManager)).getText().toString();
+            String city = ((EditText) view.findViewById(R.id.editTournamentCity)).getText().toString();
+            String country = ((EditText) view.findViewById(R.id.editTournamentCountry)).getText().toString();
+            String initDate = ((EditText) view.findViewById(R.id.editTournamentStart)).getText().toString();
+            String endDate = ((EditText) view.findViewById(R.id.editTournamentEnd)).getText().toString();
+            float prize = Float.parseFloat(((EditText) view.findViewById(R.id.editTournamentPrize)).getText().toString());
+            double longitude = Double.parseDouble(((TextView) view.findViewById(R.id.editTournamentLong)).getText().toString());
+            double latitude = Double.parseDouble(((TextView) view.findViewById(R.id.editTournamentLat)).getText().toString());
+
+            if (name.isBlank() || manager.isBlank() || city.isBlank() || country.isBlank() || initDate.isBlank() || endDate.isBlank())
+                return new Tournament();
+            initDate = initDate.replace("/", "-");
+            initDate = DateUtil.formatFromString(initDate, "yyyy-MM-dd", "dd-MM-yyyy");
+            endDate = endDate.replace("/", "-");
+            endDate = DateUtil.formatFromString(endDate, "yyyy-MM-dd", "dd-MM-yyyy");
+            if(initDate.isBlank() || endDate.isBlank()) return new Tournament();
+            if(!DateUtil.checkDates(initDate, endDate)) return new Tournament();
+
+            String address = city + ", " + country;
+            return new Tournament(name, initDate, endDate, prize, address, manager, latitude, longitude);
+        } catch (NumberFormatException | NullPointerException | ParseException e) {
+            return new Tournament();
+        }
+    }
+}
--- app/src/main/java/com/svalero/tournaments/view/TournamentFormView.java
+++ app/src/main/java/com/svalero/tournaments/view/TournamentFormView.java
@@ -20,6 +20,7 @@ import com.svalero.tournaments.interfaces.OnCoordinatesUpdatedListener;
 import com.svalero.tournaments.presenter.TournamentAddPresenter;
 import com.svalero.tournaments.presenter.TournamentModifyPresenter;
 import com.svalero.tournaments.util.DateUtil;
+import com.svalero.tournaments.util.ValidateUtil;
 
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -64,7 +65,7 @@ public class TournamentFormView extends AppCompatActivity implements OnCoordinat
     }
 
     public void onClickSaveTournament(View view){
-        Tournament tournament = validateTournamentForm();
+        Tournament tournament = ValidateUtil.validateTournamentForm(findViewById(R.id.main));
         if(tournament.getName() == null) {
             String message = getString(R.string.missing_fields);
             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
@@ -105,34 +106,6 @@ public class TournamentFormView extends AppCompatActivity implements OnCoordinat
     public void showSuccessMessage(String message) {
         action = "";
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-
-    public Tournament validateTournamentForm() throws NullPointerException, NumberFormatException{
-        try {
-            String name = ((EditText) findViewById(R.id.editTournamentName)).getText().toString();
-            String manager = ((EditText) findViewById(R.id.editTournamentManager)).getText().toString();
-            String city = ((EditText) findViewById(R.id.editTournamentCity)).getText().toString();
-            String country = ((EditText) findViewById(R.id.editTournamentCountry)).getText().toString();
-            String initDate = ((EditText) findViewById(R.id.editTournamentStart)).getText().toString();
-            String endDate = ((EditText) findViewById(R.id.editTournamentEnd)).getText().toString();
-            float prize = Float.parseFloat(((EditText) findViewById(R.id.editTournamentPrize)).getText().toString());
-            double longitude = Double.parseDouble(((TextView) findViewById(R.id.editTournamentLong)).getText().toString());
-            double latitude = Double.parseDouble(((TextView) findViewById(R.id.editTournamentLat)).getText().toString());
-
-            if (name.isBlank() || manager.isBlank() || city.isBlank() || country.isBlank() || initDate.isBlank() || endDate.isBlank())
-                return new Tournament();
-            initDate = initDate.replace("/", "-");
-            initDate = DateUtil.formatFromString(initDate, "yyyy-MM-dd", "dd-MM-yyyy");
-            endDate = endDate.replace("/", "-");
-            endDate = DateUtil.formatFromString(endDate, "yyyy-MM-dd", "dd-MM-yyyy");
-            if(initDate.isBlank() || endDate.isBlank()) return new Tournament();
-            if(!DateUtil.checkDates(initDate, endDate)) return new Tournament();
-
-            String address = city + ", " + country;
-            return new Tournament(name, initDate, endDate, prize, address, manager, latitude, longitude);
-        } catch (NumberFormatException | NullPointerException | ParseException e) {
-            return new Tournament();
-        }
     }
 
     private void loadData(Tournament tournament){
